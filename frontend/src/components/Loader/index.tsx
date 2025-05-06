4bd0632b420cfd21069fa0a0a4b1abeca5a30f42
ResendeAndrey <resende.andrey@yahoo.com.br>
2025-05-06 13:43:45 -0300

    spinner color

--- frontend/src/components/Loader/index.tsx
+++ frontend/src/components/Loader/index.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 const LoadingSpinner: React.FC = () => {
   return (
     <div className="w-full flex justify-center items-center py-8">
-      <div className="animate-spin rounded-full h-10 w-10 border-4 border-t-transparent border-blue-500"></div>
+      <div className="animate-spin rounded-full h-10 w-10 border-4 border-t-transparent border-amber-500"></div>
     </div>
   );
 };
